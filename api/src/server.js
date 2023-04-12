c0e95509e1e9c3c451b2874cefd18d476a37e68d
devlucascardoso <lucascardoso946@gmail.com>
2023-04-11 23:36:46 -0300

    add cors

--- api/src/server.js
+++ api/src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const uploadConfig = require('./configs/upload');
 
+const cors = require('cors');
 const routes = require('./routes');
 const express = require('express');
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
