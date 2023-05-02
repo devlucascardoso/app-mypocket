b07d0923772db2641213723d22840c67041984d6
devlucascardoso <lucascardoso946@gmail.com>
2023-05-01 21:49:59 -0300

    refactor userAvatarController to save user images in the database

--- api/src/controllers/UserAvatarController.js
+++ api/src/controllers/UserAvatarController.js
@@ -1,28 +1,6 @@
-const sqlite = require('sqlite')
-const { open } = require('sqlite')
-const path = require('path')
-const multer = require('multer')
-const crypto = require('crypto')
-
-const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
-const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
-
-const MULTER = {
-  storage: multer.diskStorage({
-    destination: TMP_FOLDER,
-    filename (request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex')
-      const fileName = `${fileHash}-${file.originalname}`
-
-      return callback(null, fileName)
-    }
-  })
-}
-
-const dbPromise = open({
-  filename: path.join(__dirname, '..', '..', 'db.sqlite'),
-  driver: sqlite.Database
-})
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
+const DiskStorage = require('../providers/DiskStorage')
 
 class UserAvatarController {
   async update (request, response) {
@@ -31,8 +9,7 @@ class UserAvatarController {
 
     const diskStorage = new DiskStorage()
 
-    const db = await dbPromise
-    const user = await db.get('SELECT * FROM users WHERE id = ?', user_id)
+    const user = await knex('users').where({ id: user_id }).first()
 
     if (!user) {
       throw new AppError('User not auth', 401)
@@ -45,7 +22,7 @@ class UserAvatarController {
     const filename = await diskStorage.saveFile(avatarFilename)
     user.avatar = filename
 
-    await db.run('UPDATE users SET avatar = ? WHERE id = ?', [filename, user_id])
+    await knex('users').update(user).where({ id: user_id })
 
     return response.json(user)
   }
