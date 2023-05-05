3ecf9f3f1e052c3221c5349a50b0ef377c1489ce
devlucascardoso <lucascardoso946@gmail.com>
2023-05-04 23:06:10 -0300

    fix name

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  apps: [{
-    app: 'app',
-    script: './src/server.js',
-    instances: 'max',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: 'app',
+      script: './src/server.js',
+      instances: 'max',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }]
+  ]
 }
