0a9d76a23d711b15b4780b021847753c662f3dba
devlucascardoso <lucascardoso946@gmail.com>
2023-05-04 23:09:52 -0300

    fix the semantics of the users folder to user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 
-const usersRoutes = require('./users.routes')
+const userRoutes = require('./user.routes')
 const notesRoutes = require('./notes.routes')
 const tagsRoutes = require('./tags.routes')
 const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
-routes.use('/users', usersRoutes)
+routes.use('/users', userRoutes)
 routes.use('/notes', notesRoutes)
 routes.use('/tags', tagsRoutes)
 routes.use('/sessions', sessionsRoutes)
--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -14,6 +14,11 @@ const userAvatarController = new UserAvatarController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update
+)
 
 module.exports = usersRoutes
